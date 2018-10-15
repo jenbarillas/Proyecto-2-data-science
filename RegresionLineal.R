dfb91b4eca4d8a0eda58343707778e5e3456f289
luisa2125 <luiyi2125@gmail.com>
2018-10-14 19:58:20 -0600

    REGRESION LINEAL POR AVG YEAR

--- RegresionLineal.R
+++ RegresionLineal.R
@@ -1,5 +1,5 @@
-prediccion<-function(arreglo, year=TRUE, month=FALSE){
-  if (year) {
+prediccion<-function(arreglo, month=FALSE){
+  if (!month) {
     dat<-arreglo[c('mp_year', 'mp_price')]
     arreglo<-aggregate( mp_price ~ mp_year, dat, mean )
     cor.test(x = arreglo$mp_year, y = arreglo$mp_price, method = "pearson")
@@ -14,8 +14,107 @@ prediccion<-function(arreglo, year=TRUE, month=FALSE){
 
 datos<-read.csv('./foodPricesClean.csv')
 # head(datos)
+prediccionesGuatemala<-data.frame()
 datosGuatemala<-subset(datos,adm0_name == 'Guatemala')
 # head(datosGuatemala)
 bananas<-subset(datosGuatemala,cm_name == 'Bananas')
-
 bananas<-prediccion(bananas)
+bananas['cm_name'] = 'Bananas'
+prediccionesGuatemala<-rbind(prediccionesGuatemala, setNames(bananas, names(bananas)))
+
+
+beans<-subset(datosGuatemala,cm_name == 'Beans')
+beans<-prediccion(beans)
+beans['cm_name'] = 'Beans'
+prediccionesGuatemala<-rbind(prediccionesGuatemala, setNames(beans, names(prediccionesGuatemala)))
+
+
+panes<-subset(datosGuatemala, cm_name == 'Bread')
+panes<-prediccion(panes)
+panes['cm_name'] = 'Bread'
+prediccionesGuatemala<-rbind(prediccionesGuatemala, setNames(panes, names(prediccionesGuatemala)))
+
+
+cheese<-subset(datosGuatemala,cm_name == 'Cheese')
+cheese<-prediccion(cheese)
+cheese['cm_name'] = 'Cheese'
+prediccionesGuatemala<-rbind(prediccionesGuatemala, setNames(cheese, names(prediccionesGuatemala)))
+
+
+coffee<-subset(datosGuatemala,cm_name == 'Coffee')
+coffee<-prediccion(coffee)
+coffee['cm_name'] = 'Coffee'
+prediccionesGuatemala<-rbind(prediccionesGuatemala, setNames(coffee, names(prediccionesGuatemala)))
+
+
+eggs<-subset(datosGuatemala,cm_name == 'Eggs')
+eggs<-prediccion(eggs)
+eggs['cm_name'] = 'Eggs'
+prediccionesGuatemala<-rbind(prediccionesGuatemala, setNames(eggs, names(prediccionesGuatemala)))
+
+fuel<-subset(datosGuatemala,cm_name == 'Fuel (petrol-gasoline)')
+fuel<-prediccion(fuel)
+fuel['cm_name'] = 'Fuel'
+prediccionesGuatemala<-rbind(prediccionesGuatemala, setNames(fuel, names(prediccionesGuatemala)))
+
+meat<-subset(datosGuatemala,cm_name == 'Meat (beef, chops with bones)')
+meat<-prediccion(meat)
+meat['cm_name'] = 'Meat'
+prediccionesGuatemala<-rbind(prediccionesGuatemala, setNames(meat, names(prediccionesGuatemala)))
+
+chicken<-subset(datosGuatemala,cm_name == 'Meat (chicken)')
+chicken<-prediccion(chicken)
+chicken['cm_name'] = 'Chicken'
+prediccionesGuatemala<-rbind(prediccionesGuatemala, setNames(chicken, names(prediccionesGuatemala)))
+
+milk<-subset(datosGuatemala,cm_name == 'Milk')
+milk<-prediccion(milk)
+milk['cm_name'] = 'Milk'
+prediccionesGuatemala<-rbind(prediccionesGuatemala, setNames(milk, names(prediccionesGuatemala)))
+
+oil<-subset(datosGuatemala,cm_name == 'Oil (vegetable)')
+oil<-prediccion(oil)
+oil['cm_name'] = 'Oil'
+prediccionesGuatemala<-rbind(prediccionesGuatemala, setNames(oil, names(prediccionesGuatemala)))
+
+pasta<-subset(datosGuatemala,cm_name == 'Pasta')
+pasta<-prediccion(pasta)
+pasta['cm_name'] = 'Pasta'
+prediccionesGuatemala<-rbind(prediccionesGuatemala, setNames(pasta, names(prediccionesGuatemala)))
+
+plantains<-subset(datosGuatemala,cm_name == 'Plantains')
+plantains<-prediccion(plantains)
+plantains['cm_name'] = 'Plantains'
+prediccionesGuatemala<-rbind(prediccionesGuatemala, setNames(plantains, names(prediccionesGuatemala)))
+
+potatoes<-subset(datosGuatemala,cm_name == 'Potatoes')
+potatoes<-prediccion(potatoes)
+potatoes['cm_name'] = 'Potatoes'
+prediccionesGuatemala<-rbind(prediccionesGuatemala, setNames(potatoes, names(prediccionesGuatemala)))
+
+rice<-subset(datosGuatemala,cm_name == 'Rice (ordinary, second quality)')
+rice<-prediccion(rice)
+rice['cm_name'] = 'Rice'
+prediccionesGuatemala<-rbind(prediccionesGuatemala, setNames(rice, names(prediccionesGuatemala)))
+
+salt<-subset(datosGuatemala,cm_name == 'Salt')
+salt<-prediccion(salt)
+salt['cm_name'] = 'Salt'
+prediccionesGuatemala<-rbind(prediccionesGuatemala, setNames(salt, names(prediccionesGuatemala)))
+
+sugar<-subset(datosGuatemala,cm_name == 'Sugar')
+sugar<-prediccion(sugar)
+sugar['cm_name'] = 'Sugar'
+prediccionesGuatemala<-rbind(prediccionesGuatemala, setNames(sugar, names(prediccionesGuatemala)))
+
+tomatoes<-subset(datosGuatemala,cm_name == 'Tomatoes')
+tomatoes<-prediccion(tomatoes)
+tomatoes['cm_name'] = 'Tomatoes'
+prediccionesGuatemala<-rbind(prediccionesGuatemala, setNames(tomatoes, names(prediccionesGuatemala)))
+
+tortillas<-subset(datosGuatemala,cm_name == 'Tortilla (maize)')
+tortillas<-prediccion(tortillas)
+tortillas['cm_name'] = 'Tortillas'
+prediccionesGuatemala<-rbind(prediccionesGuatemala, setNames(tortillas, names(prediccionesGuatemala)))
+
+
